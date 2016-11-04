65476d43cfe74f5185ddfc6246364e6dd1fc427f
Jiří Fatka <fatkaj@ntis.zcu.cz>
2016-11-04 15:41:34 +0100

    Interface and implementation separation.
    This separation allows to better hide implementation from interface.

--- src/plugin/Manager.cpp
+++ src/plugin/Manager.cpp
@@ -24,7 +24,7 @@
 /* ************************************************************************ */
 
 // Declaration
-#include "Manager.hpp"
+#include "cece/plugin/Manager.hpp"
 
 // C++
 #include <utility>
